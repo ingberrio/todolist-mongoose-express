9b1d503db5f904a48248c111459bd136dd27cf4a
Eduard Berrio <genial@duck.com>
2023-08-13 13:26:31 -0500

    new route to dele  items

--- app.js
+++ app.js
@@ -70,6 +70,24 @@ app.post("/", async function(req, res) {
   }
 });
 
+// Route to handle adding a new item
+app.post("/delete", async function(req, res) {
+  try {
+    // Extract the new item's id checkbox from the request body
+    const listName = req.body.listItem;
+    
+    // Delete a item using the Item model
+    await Item.deleteOne({ _id: listName });
+
+    console.log("Item deleted successfully");
+    // Redirect to the home page
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error delete item:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // Route to render the "about" page
 app.get("/about", function(req, res) {
   res.render("about");
