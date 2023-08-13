f62a5f810d34d6c07ab94fb73ea9b449a060f970
Eduard Berrio <genial@duck.com>
2023-08-13 11:10:36 -0500

    new route to save items

--- app.js
+++ app.js
@@ -1,78 +1,76 @@
-//jshint esversion:6
-
+// Import required packages
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+// Create an instance of Express app
 const app = express();
 
+// Set view engine to EJS
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+// Use bodyParser middleware to parse URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the "public" directory
 app.use(express.static("public"));
 
+// Define the item schema for the MongoDB model
 const itemSchema = {
   name: String,
-}
+};
 
-const Item = mongoose.model("item", itemSchema)
+// Create an "Item" model based on the itemSchema
+const Item = mongoose.model("item", itemSchema);
 
-const items = [
-  {
-    name:"Buy milk",
-  },
-  {
-    name : "Go to the gym"
-  },
-  {
-    name :"Eat breakfast"
-  }
-]
+// Connect to the MongoDB database
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("Connected to MongoDB");
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true})
-
-.then(async () => {
-
-  try {
-    // Insert the names array into the "items" collection
-    await Item.insertMany(items);
-
-    // Fetch task name in view home
-    let itemsFound = await Item.find({});
-    app.get("/", function(req, res) {
-      res.render("list", { listTitle: "Today", newListItems: itemsFound });
-    });
-    
-    console.log('Documents inserted successfully');
-    
-    mongoose.connection.close();
-    } catch (err) {
-      console.error('Error inserting documents:', err);
-    }
-  })
-  .catch((error) => {
-    console.error('Error al conectar a MongoDB:', error); // Connection error message
+  // Start the Express server
+  app.listen(3000, function() {
+    console.log("Server started on port 3000");
   });
+})
+.catch((error) => {
+  console.error('Error to connect MongoDB:', error);
+});
 
-
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+// Route to render the home page
+app.get("/", async function(req, res) {
+  try {
+    // Fetch all items from the database
+    const itemsFound = await Item.find({});
+    // Render the "list" view with the fetched items
+    res.render("list", { listTitle: "Today", newListItems: itemsFound });
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
+// Route to handle adding a new item
+app.post("/", async function(req, res) {
+  try {
+    // Extract the new item's name from the request body
+    let itemName = req.body.newItem;
+    // Create a new item using the Item model
+    await Item.create({ name: itemName });
 
-app.get("/about", function(req, res){
-  res.render("about");
+    console.log("Item saved successfully");
+    // Redirect to the home page
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error saving item:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+// Route to render the "about" page
+app.get("/about", function(req, res) {
+  res.render("about");
 });
